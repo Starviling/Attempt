2c3ad9b10ee936e73975955f8b0c13d9d311a414
henry <henry@DESKTOP-2D0AVCU>
2019-05-16 20:59:18 -0400

    Ore generation

---
+++ src/main/java/starhnl1/world/OreGeneration.java
@@ -0,0 +1,24 @@
+package starhnl1.world;
+
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage.Decoration;
+import net.minecraft.world.gen.feature.CompositeFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.MinableConfig;
+import net.minecraft.world.gen.placement.CountRange;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraftforge.registries.ForgeRegistries;
+import starhnl1.lists.BlockList;
+
+public class OreGeneration 
+{
+	public static void setupOreGeneration()
+	{
+		for(Biome biome : ForgeRegistries.BIOMES)
+		{
+			CountRangeConfig stuff_ore_placement = new CountRangeConfig(5, 20, 20, 100);
+			biome.addFeature(Decoration.UNDERGROUND_ORES, new CompositeFeature<>(Feature.MINABLE, new MinableConfig(MinableConfig.IS_ROCK, BlockList.copper_ore.getDefaultState(), 7), new CountRange(), stuff_ore_placement));
+		}
+	}
+}
